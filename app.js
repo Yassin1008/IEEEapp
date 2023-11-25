ec70ebef3e32ebb77a4f17be511a794a54683947
yassin mohamed <yassinmohamed007@gmail.com>
2023-11-25 11:26:33 +0200

    Added package.json

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+const specificeventID = 420;
+
+app.get(`/events/${specificeventID}`,(req,res) => {
+
+    res.status(200).json({message: 'Welcome to our website!!'});
+});
+
+const PORT = 7000;
+
+app.listen(PORT,() =>{
+    console.log(`Connected to port ${PORT}`);
+} )
